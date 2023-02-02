758e4666809e200d5816c6e13587f1b6580e2105
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-02-02 19:48:58 +0200

    added variables and accessors for HummingBirdAgent

--- Assets/Hummingbird/Scripts/HummingBirdAgent.cs
+++ Assets/Hummingbird/Scripts/HummingBirdAgent.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents;
 using UnityEngine;
 
-public class HummingBirdAgent : MonoBehaviour
+/// <summary>
+/// A hummingbird Machine Learning Agent
+/// </summary>
+public class HummingBirdAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Tooltip("Force to apply when moving")]
+    public float moveForce = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Tooltip("Speed to pitch up or down")]
+    public float pitchSpeed = 100f;
+
+    [Tooltip("Speed to rotate around the up axis")]
+    public float yawSpeed = 100f;
+
+    [Tooltip("Transform at the tip of the beak")]
+    public Transform beakTip;
+
+    [Tooltip("The agent's camera")]
+    public Camera agentCamera;
+
+    [Tooltip("Whether this is training mode or gameplay mode")]
+    public bool trainingMode;
+
+    // The rigidbody of the agent
+    new private Rigidbody rigidbody;
+
+    // The flower area that the agent is in
+    private FlowerArea flowerArea;
+
+    // The nearest flower to the agent
+    private Flower nearestFlower;
+
+    // Allows for smoother pitch changes
+    private float smoothPitchChange = 0f;
+
+    // Allow for smoother yaw changes
+    private float smoothYawChange = 0f;
+
+    // Maximum angle that the bird can pitch up or down
+    private const float MaxPitchAngle = 80f;
+
+    // Maximum distance from the beak tip to accept nectar collision
+    private const float BeakTipRadius = 0.008f;
+
+    // Whether the agent is frozen (intentionally not flying)
+    private bool frozen = false;
+
+    /// <summary>
+    /// The amount of nectar the agent has obtained this episode
+    /// </summary>
+    public float NectarObtained { get; private set; }
 }
