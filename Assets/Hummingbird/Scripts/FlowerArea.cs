3be2967fdee7255eeb01a4304767d49f96bddf49
Nafets94 <45308745+Nafets94@users.noreply.github.com>
2023-02-02 17:03:55 +0200

    Update FlowerArea.cs

--- Assets/Hummingbird/Scripts/FlowerArea.cs
+++ Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -2,17 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages a collection of flower plants and attached flowers
+/// </summary>
 public class FlowerArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // The diameter of the area where the agent and flowers can be
+    // used for observing relative distance from agent to flower
+    public const float AreaDiameter = 20f;
+
+    // The list of all flower plants in this flower area (flower plants have multiple flowers)
+    private List<GameObject> flowerPlants;
+
+    // A lookup dictionary for looking up a flower from a nectar collider
+    private Dictionary<Collider, Flower> nectarFlowerDictionary;
+
+    /// <summary>
+    /// The list of all flowers in the flower area
+    /// </summary>
+    public List<Flower> Flowers { get; private set; }
+
+    /// <summary>
+    /// Reset the flowers and flower plants
+    /// </summary>
+    public void ResetFlowers()
     {
-        
+        // Rotate each flower plant around the Y axis and subtly around X and Z
+        foreach(GameObject flowerPlant in flowerPlants)
+        {
+            float xRotation = UnityEngine.Random.Range(-5f, 5f);
+            float yRotation = UnityEngine.Random.Range(-180f, 180f);
+            float zRotation = UnityEngine.Random.Range(-5f, 5f);
+
+            flowerPlant.transform.localRotation = Quaternion.Euler(zRotation, xRotation, yRotation);
+        }
+
+        // Reset each flower
+        foreach (Flower flower in Flowers)
+        {
+            flower.ResetFlower();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Gets the <see cref="Flower"/> that a nectar collider belongs to
+    /// </summary>
+    /// <param name="collider">The nectar collider</param>
+    /// <returns>The matching flower</returns>
+    public Flower GetFlowerFromNectar(Collider collider)
     {
-        
+        return nectarFlowerDictionary[collider];
+    }
+
+    /// <summary>
+    /// Called when the area wakes up
+    /// </summary>
+    private void Awake()
+    {
+        // Initialize variables
+        flowerPlants = new List<GameObject>();
+        nectarFlowerDictionary = new Dictionary<Collider, Flower>();
+        Flowers = new List<Flower>();
+    }
+
+    /// <summary>
+    /// Called when the game starts
+    /// </summary>
+    private void Start()
+    {
+        // Find all flowers that are children of this GameObject/Transform
+        FindChildFlowers(transform);
     }
 }
